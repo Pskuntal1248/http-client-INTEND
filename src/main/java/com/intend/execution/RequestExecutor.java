1680514d4026a93b869208351157a1468c367c91
Parth singh <ps.kuntal2005@gmail.com>
2026-03-03 23:29:35 +0530

    Change execute method return type to ExecutionResult in RequestExecutor interface

--- src/main/java/com/intend/execution/RequestExecutor.java
+++ src/main/java/com/intend/execution/RequestExecutor.java
@@ -4,5 +4,5 @@ import com.intend.core.RequestIntent;
 import java.util.Map;
 
 public interface RequestExecutor {
-    String execute(RequestIntent intent, Map<String, String> headers);
+    ExecutionResult execute(RequestIntent intent, Map<String, String> headers);
 }
