5f5dca3d4670d3a835a2c63eab2172590296dc5a
Parth singh <ps.kuntal2005@gmail.com>
2026-03-07 02:21:52 +0530

    Remove CommandLineRunner and related logic from IntendApplication

--- src/main/java/com/intend/IntendApplication.java
+++ src/main/java/com/intend/IntendApplication.java
@@ -1,38 +1,7 @@
 package com.intend;
 
-import com.intend.controller.cli.IntendCommand;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import picocli.CommandLine;
-import picocli.CommandLine.IFactory;
 
 @SpringBootApplication
-public class IntendApplication implements CommandLineRunner, ExitCodeGenerator {
-
-    private final IntendCommand command;
-    private final IFactory factory;
-    private int exitCode;
-
-    public IntendApplication(IntendCommand command, IFactory factory) {
-        this.command = command;
-        this.factory = factory;
-    }
-
-    public static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(IntendApplication.class, args)));
-    }
-
-    @Override
-    public void run(String... args) {
-        if (args.length > 0) {
-            exitCode = new CommandLine(command, factory).execute(args);
-        }
-    }
-
-    @Override
-    public int getExitCode() {
-        return exitCode;
-    }
+public class IntendApplication {
 }
