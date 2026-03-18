fdaf88220fb78a20efef15a7b62cbdf6c6d362b9
Parth singh <ps.kuntal2005@gmail.com>
2026-03-18 23:10:03 +0530

    Refactor EngineConfigTest: remove in-memory StateRepository implementation for cleaner test setup

--- src/test/java/com/intend/config/EngineConfigTest.java
+++ src/test/java/com/intend/config/EngineConfigTest.java
@@ -1,12 +1,8 @@
 package com.intend.config;
 
 import com.intend.engine.HeaderEngine;
-import com.intend.repository.StateRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,22 +14,7 @@ class EngineConfigTest {
     void headerEngineBeanCreated() {
         EngineConfig config = new EngineConfig();
 
-        // Simple in-memory StateRepository for test
-        StateRepository stateRepository = new StateRepository() {
-            private final Map<String, String> store = new HashMap<>();
-
-            @Override
-            public String getLastIdempotencyKey(String key) {
-                return store.get(key);
-            }
-
-            @Override
-            public void saveIdempotencyKey(String key, String uuid) {
-                store.put(key, uuid);
-            }
-        };
-
-        HeaderEngine engine = config.headerEngine(stateRepository);
+        HeaderEngine engine = config.headerEngine();
         assertThat(engine).isNotNull();
     }
 }
