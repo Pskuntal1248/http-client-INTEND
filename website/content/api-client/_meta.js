d571ef09192770a3f5cc7d460bdf005d62e80a35
Parth singh <ps.kuntal2005@gmail.com>
2026-03-19 14:36:47 +0530

    feat: Rework website content structure, introducing new API client, deep-dive, getting started, and quick start sections while updating core application classes.

---
+++ website/content/api-client/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  "overview": "API Client Overview",
+  "getting-started": "Getting Started (Requests)",
+  "sending-requests": "Sending Requests & Uploads",
+  "organizing-apis": "Organizing APIs: History",
+  "variables-environments": "Variables & Environments",
+  "chaining-responses": "Chaining & Responses"
+}
